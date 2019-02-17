8adde7ff151222651aac509b5ede46ac39eff82a
Atte Penttala <pentt.at@hotmail.com>
2019-02-18 01:50:55 +0200

    Added basic functionality for '-a' command, and some helper urls

--- server.js
+++ server.js
@@ -1,42 +1,80 @@
 'use strict';
 
 const express = require('express');
-//var exec = require("child_process").exec;
-//var http = require('http');
 var exec = require('child_process').exec;
+//var spawn = require('child_process').spawn;
+var fs = require('fs');
+var process;
 
-//create a server object:
-/*http.createServer(function (req, res) {
-    console.log(req.url);
-    exec("src/RefactoringMiner/bin/RefactoringMiner -h", function(error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-    });
-    // Wrong path
-    exec("src/build/distributions/RefactoringMiner/bin/RefactoringMiner -h", function(error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        }
-    });
-    res.write('yaas'); //write a response to the client
-    res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
-*/
+
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const app = express();
+
 app.get('/', (req, res) => {
     res.send('Specify commands\n');
 });
+app.get('/help', (req, res) => {
+    res.send('help will be added here');
+});
 app.get('/-h', (req, res) => {
     // exec code here
-    exec("src/RefactoringMiner/bin/RefactoringMiner -h", function(error, stdout, stderr) {
-        console.log(stdout);
+    var child = exec("src/RefactoringMiner/bin/RefactoringMiner -h");
+    var wstream = fs.createWriteStream('output.txt');
+    child.stdout.on('data', function(data) {
+        console.log(data);
+        wstream.write(data + "\n");
+    });
+    child.on('close', function(code) {
+        console.log(code);
+        wstream.end();
     });
     res.send('-h\n');
 });
+// Example url: http://192.168.99.100:49160/-a?url=https://github.com/Colho/swengmeth_refminer_node.git
+app.get('/-a', (req, res) => {
+    // exec code here
+    process = true;
+    var remove = exec("rm -r temp");
+    remove.on('close', function(code) {
+        console.log(code);
+        var child = exec("git clone " + req.query.url + " temp");
+        var wstream = fs.createWriteStream('output.txt');
+        child.stdout.on('data', function(data) {
+            console.log(data);
+        });
+        child.on('close', function(code) {
+            console.log(code);
+            var child2 = exec("src/RefactoringMiner/bin/RefactoringMiner -a temp")
+            child2.stdout.on('data', function(data) {
+                console.log(data);
+                wstream.write(data + "\n");
+            });
+            child2.on('close', function(code) {
+                console.log(code);
+                process = false;
+                wstream.end();
+                //res.download("temp/all_refactorings.csv");
+            });
+        });
+    });
+    res.send('-a\n');
+});
+app.get('/status', function(req, res) {
+    if (process) {
+        res.send('Mining in process, please wait\n');
+    }
+    else {
+        res.send('Mining complete, get results with /download or /downloadcsv\n');
+    }
+});
+app.get('/download', function(req, res) {
+    res.download("output.txt");
+});
+app.get('/downloadcsv', function(req, res) {
+    res.download("temp/all_refactorings.csv");
+})
 
 app.listen(PORT, HOST);
-console.log('Running on http://${HOST}:${PORT}');
+console.log('Running...');
